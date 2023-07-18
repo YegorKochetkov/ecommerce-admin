c4ca4c248f7824dcab0d0fb96f5b3bfd94b969d2
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-07-18 16:15:04 +0300

    feat: add modal component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,22 @@
+"use client";
+
+import Modal from "@/components/ui/modal";
 import { UserButton } from "@clerk/nextjs";
 
 const SetupPage = () => {
 	return (
 		<div className='p-4'>
-			<UserButton
-				afterSignOutUrl='/'
-				appearance={{
-					elements: { userButtonAvatarBox: { width: "3rem", height: "3rem" } },
+			<UserButton afterSignOutUrl='/' />
+			<Modal
+				title='Title test'
+				isOpen
+				description='description test'
+				onClose={() => {
+					console.log("close");
 				}}
-			/>
+			>
+				Children content
+			</Modal>
 		</div>
 	);
 };
