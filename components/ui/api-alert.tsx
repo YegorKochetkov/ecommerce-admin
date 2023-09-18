9a19b0e1ce0590ae7ae1cbddf9b8f05732e6fe0d
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-09-18 23:16:13 +0300

    feat: add store settings page
    user can rename, delete store

---
+++ components/ui/api-alert.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import toast from "react-hot-toast";
+import { Copy, Server } from "lucide-react";
+
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Badge, BadgeProps } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+
+type ApiAlertProps = {
+  title: string;
+  description: string;
+  variant: "public" | "admin";
+};
+
+const textMap: Record<ApiAlertProps["variant"], string> = {
+  public: "Public",
+  admin: "Admin",
+};
+
+const variantMap: Record<ApiAlertProps["variant"], BadgeProps["variant"]> = {
+  public: "secondary",
+  admin: "destructive",
+};
+
+export const ApiAlert = ({ title, description, variant }: ApiAlertProps) => {
+  const onCopy = () => {
+    navigator.clipboard.writeText(description);
+    toast.success("API Route copied to clipboard");
+  };
+
+  return (
+    <Alert className="min-w-min">
+      <Server className="w-4" />
+      <AlertTitle className="flex items-center gap-x-2">
+        {title}
+        <Badge variant={variantMap[variant]}>{textMap[variant]}</Badge>
+      </AlertTitle>
+      <AlertDescription className="mt-4 flex items-center justify-between gap-x-2">
+        <code className="fon relative rounded bg-muted px-2 py-1 font-mono text-sm font-semibold">
+          {description}
+        </code>
+        <Button size="icon" variant="outline" onClick={onCopy}>
+          <Copy className="w-4" />
+        </Button>
+      </AlertDescription>
+    </Alert>
+  );
+};
