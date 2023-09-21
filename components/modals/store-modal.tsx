646ca4aa9b6578f6f1169b59b8d53d366c40fc99
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-09-21 10:43:23 +0300

    chore: remove redundant div

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -63,43 +63,41 @@ const StoreModal = () => {
       isOpen={isOpen}
       onClose={onClose}
     >
-      <div>
-        <div className="space-y-4 py-2 pb-4">
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)}>
-              <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Name</FormLabel>
-                    <FormControl>
-                      <Input
-                        disabled={loading}
-                        placeholder="E-commerce"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <div className="flex w-full items-center justify-end space-x-2 pt-6">
-                <Button
-                  type="button"
-                  disabled={loading}
-                  variant="outline"
-                  onClick={onClose}
-                >
-                  Cancel
-                </Button>
-                <Button disabled={loading} type="submit">
-                  Continue
-                </Button>
-              </div>
-            </form>
-          </Form>
-        </div>
+      <div className="space-y-4 py-2 pb-4">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="E-commerce"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex w-full items-center justify-end space-x-2 pt-6">
+              <Button
+                type="button"
+                disabled={loading}
+                variant="outline"
+                onClick={onClose}
+              >
+                Cancel
+              </Button>
+              <Button disabled={loading} type="submit">
+                Continue
+              </Button>
+            </div>
+          </form>
+        </Form>
       </div>
     </Modal>
   );
